17b41ce640c4e38f8c39f4087482a1ca71650ccc
Connor Stauffer <cstauffer@buildzoom.com>
2015-11-08 17:48:38 -0800

    update config files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+	
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -76,4 +77,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+	config.react.variant = :production
+
 end
