4a17c0442a73dc0037960789778d6c5d1bf73372
Connor Stauffer <cstauffer@buildzoom.com>
2015-11-09 06:50:05 -0800

    add associations and validation to user and trainer

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,2 +1,5 @@
 class Trainer < ActiveRecord::Base
+
+	belongs_to :user
+	
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+
+	validates :first_name, :last_name, :email, presence: true
+	validates :email, format: { with: /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/ }
+
+	has_one :trainer
+
 end
