56389f540baed52c0ec83745b89b4c93d73af931
Connor Stauffer <cstauffer@buildzoom.com>
2015-11-12 07:50:24 -0800

    add password digest to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151112153500) do
+ActiveRecord::Schema.define(version: 20151112154919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,11 +63,12 @@ ActiveRecord::Schema.define(version: 20151112153500) do
   add_index "trainers", ["user_id"], name: "index_trainers_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "first_name", null: false
-    t.string   "last_name",  null: false
-    t.string   "email",      null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "first_name",      null: false
+    t.string   "last_name",       null: false
+    t.string   "email",           null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest", null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,12 +2,13 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  first_name :string           not null
-#  last_name  :string           not null
-#  email      :string           not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :integer          not null, primary key
+#  first_name      :string           not null
+#  last_name       :string           not null
+#  email           :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string           not null
 #
 
 require 'test_helper'
